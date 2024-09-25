131869fbdb4b304cc32deca648439c608085bb34
Hugo <hhn69969@educastur.es>
2024-09-25 11:02:43 +0200

    Clase customer terminada

--- MyLibrary/Customer.cs
+++ MyLibrary/Customer.cs
@@ -8,5 +8,11 @@ namespace MyLibrary
 {
     internal class Customer
     {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public String FullName => $"{FirstName} {LastName}";
+        
+        
     }
 }
